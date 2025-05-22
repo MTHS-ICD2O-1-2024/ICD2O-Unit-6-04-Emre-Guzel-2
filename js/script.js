26d4665bff7f0a9e3130bb77d8654fb535c7e21a
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-05-22 19:11:30 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Emre Guzel
+// Created on: April 2 2025
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// Setiing the functoin 
+function volumeOfSphere() {
+
+  // Setting the varibles
+
+  document.getElementById("Volume").innerHTML =
+  "Volume is: " + volumeOfSphere.toFixed(2)
+}
