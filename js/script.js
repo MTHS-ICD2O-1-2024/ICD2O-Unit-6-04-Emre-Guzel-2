2790f1cb7d6972c44f4bce625533f4cfb759de82
Emre-Guzel-2 <yunus.emre.guzel@mths.ca>
2025-05-22 20:00:14 -0400

    finshed the prgram

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: Emre Guzel
-// Created on: April 2 2025
+// Created on: May 22 2025
 // This file contains the JS functions for index.html
 
 "use strict"
 
 // Setiing the functoin 
-function volumeOfSphere() {
+window.onload = function () {
+  const params = new URLSearchParams(document.location.search)
 
   // Setting the varibles
+  const radius = params.get("radius")
+  console.log(radius)
 
-  document.getElementById("Volume").innerHTML =
-  "Volume is: " + volumeOfSphere.toFixed(2)
+  // Calculatosin 
+  const volume = (4 / 3) * Math.PI * Math.pow(radius, 3)
+  const dimensions =
+    "<ul>\n<li> Volume is " +
+    volume.toFixed(2) + " " + "cm³ " +
+    "</li>\n</ul>"
+
+  // Showing the asnwer 
+  document.getElementById("Volume").innerHTML = dimensions
+  "Volume is: " + volume.toFixed(2)
 }
